54f54748b3921b66527940c5a82daf551d6bc537
ByeoungJoon Jeon <juni8453@naver.com>
2023-01-30 19:55:02 +0900

    :sparkles: Event 및 관련 클래스 생성
    ## Description
    - Event, EventStatus 클래스 생성

---
+++ src/main/java/com/practice/rest/event/domain/Event.java
@@ -0,0 +1,35 @@
+package com.practice.rest.event.domain;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@Builder
+public class Event {
+
+  private Integer id;
+  private String name;
+  private String description;
+  private LocalDateTime beginEnrollmentDateTime;
+  private LocalDateTime closeEnrollmentDateTime;
+  private LocalDateTime beginEventDateTime;
+  private LocalDateTime endEventDateTime;
+  private String location;
+  private int basePrice;
+  private int maxPrice;
+  private int limitOfEnrollment;
+  private boolean offline;
+  private boolean free;
+  private EventStatus eventStatus;
+
+}
---
+++ src/main/java/com/practice/rest/event/domain/EventStatus.java
@@ -0,0 +1,6 @@
+package com.practice.rest.event.domain;
+
+public enum EventStatus {
+
+  DRAFT, PUBLISHED, BEGAN_ENROLLMENT;
+}
